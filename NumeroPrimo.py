bfe7a67b2bfa0e6ba52baf49adfe8c055a3d5c61
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-08-17 02:46:16 -0500

    Se optimiza el cálculo del primo, dividiendo solamente hasta la raíz de N.

--- NumeroPrimo.py
+++ NumeroPrimo.py
@@ -1,11 +1,13 @@
 # Programa que identifique si un número es primo
+import math
+
 print("PROGRAMA QUE DETERMINA SI UN NÚMERO ES PRIMO")
 print("Ingresa el número")
 numero = int(input())
 
 esPrimo = True
 i = 2
-while i < numero and esPrimo == True:
+while i <= math.sqrt(numero):
     if numero % i == 0:
         esPrimo = False
     i += 1
